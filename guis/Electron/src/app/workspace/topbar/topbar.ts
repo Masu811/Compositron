03a1736bad7b6ae459dc42492255589851892a0f
Maximilian Suhr <ge96quf@mytum.de>
2026-08-23 08:32:14 +0200

    Split off TS gui into another project

--- guis/Electron/src/app/workspace/topbar/topbar.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "topbar",
-  templateUrl: "topbar.html",
-  styleUrl: "topbar.css",
-})
-export class TopBar { }
--- guis/Electron/src/electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  showOpenDialog: () => ipcRenderer.invoke("showOpenDialog"),
-  showSaveDialog: () => ipcRenderer.invoke("showSaveDialog"),
-  health: () => ipcRenderer.invoke("health"),
-});
